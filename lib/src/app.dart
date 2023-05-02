b55583d0dad27ecb7c4b64931013bdc256b16925
HaroldOsorioFlores <haroldosorio32@gmail.com>
2023-05-01 20:23:34 -0500

    se configuro la estructuracion de carpetas

---
+++ lib/src/app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:pethome_connect/src/screens/home/home_page.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'PetHome Connect',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Center(child: HomePage()),
+    );
+  }
+}
